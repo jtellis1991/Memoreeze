143dfe78b0fd96ec51e750015a0a5385bc693d3b
jtellis1991 <joshuaellis1991@gmail.com>
2021-08-18 17:33:37 +0000

    updated association for Assignment model, need to get associations for floating working

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -14,6 +14,7 @@
 class Assignment < ApplicationRecord
   belongs_to :course,  required: false, class_name: "Course", foreign_key: :course_id 
   belongs_to :deck, required: false, class_name: "Deck", foreign_key: :deck_id 
+  belongs_to :user, required: true, class_name: "User", foreign_key: :user_id
 
   has_many(:grades, { :class_name => "Grade", :foreign_key => "assignment_id", :dependent => :destroy })
 
